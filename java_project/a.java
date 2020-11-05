cb581bc814a9d4fcd7b3cf026b488c7bd0bd854e
vashwini17 <59556843+vashwini17@users.noreply.github.com>
2020-11-06 00:40:27 +0530

    Add files via upload

---
+++ java_project/a.java
@@ -0,0 +1,54 @@
+
+
+package com.tutorialspoint;
+
+import java.io.*;
+import java.util.*;
+public class Test
+ {
+   public static void main(String args[])throws Exception
+   {
+     File F1 = new File("/home/oracle/ashu/a.txt");
+     BufferedReader br = new BufferedReader(new FileReader(F1));
+
+ String st;
+     while ((st = br.readLine()) != null)
+     System.out.println(st);
+    }
+
+    {
+   int[][] twoD_arr = new int[20][2];
+
+       System.out.println("elements A.txt:");
+       for(int i=0; i<= 20; i++)
+    {
+            for(int j=0; j<=2; j++)
+           {
+                System.out.println(twoD_arr[i][j] + "\t");
+            }
+          System.out.println("");
+     }
+  }
+}
+
+
+
+{
+   File F1 = new File("/home/oracle/ashu/b.text");
+   bufferReader br = new BufferReader(new FileReader(F1));
+
+   String st;
+   while ((st = br.readline()) != null)
+   system.out.println(st);
+}
+
+
+
+
+
+optioption = new Hashtable();
+
+     htable2.put(1, "A");
+     htable2.put(2, "B");
+     htable2.put(4, "M");
+Type  :quit<Enter>  to exit Vim                                                                                                                                                                  1,1           Top
